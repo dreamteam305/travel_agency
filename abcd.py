f07e024224551963b23a324771aa0bdd2ba436b0
Bogdan Oleinikov <bogdan.oleinikov@stonybrook.edu>
2018-05-03 02:27:49 -0400

    start of SQLalchemy

--- abcd.py
+++ abcd.py
@@ -1,6 +1,9 @@
 from flask import*
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 @app.route('/')
