e2d868ee04081843d1537e1691715c1c6544afdb
nafi <nafiul.bzsb@gmail.com>
2018-05-03 02:26:12 -0400

    css and javascript loading is fixed! The server now can handle any html files as well

--- abcd.py
+++ abcd.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def main():
    return render_template('index.html')
 
+
+@app.route("/<filename>.html")
+def htmlRoute(filename):
+   return render_template(filename+".html")
+
+
 if __name__ == "__main__":
    app.run(debug= True)
 
