0f9d415fae903e3afe18f983d73007c3cbfbce31
Bogdan Oleinikov <bogdan.oleinikov@stonybrook.edu>
2018-05-06 18:49:04 -0400

    added one comment

--- abcd.py
+++ abcd.py
@@ -10,7 +10,7 @@ mysql = MySQL()
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Jamal1994'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'bogdan322464'
 app.config['MYSQL_DATABASE_DB'] = 'travel_agency'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
@@ -22,8 +22,51 @@ conn = mysql.connect()
 def a():
    return "<form action=>"
 
+#main path
 @app.route('/')
 def main():
+   flight_carrier = mysql.connect().cursor()
+   flight_carrier.execute("SELECT distinct Flight_Carrier FROM `travel_agency`.`flight`;")
+   # flight_carr= flight_carrier.fetchall()
+
+   flight_source = mysql.connect().cursor()
+   flight_source.execute("SELECT distinct Source_Airport_ID FROM `travel_agency`.`flight`;")
+
+   flight_destination = mysql.connect().cursor()
+   flight_destination.execute("SELECT distinct Destination_Airport_ID FROM `travel_agency`.`flight`;")
+
+   flight_clas = mysql.connect().cursor()
+   flight_clas.execute("SELECT distinct Flight_Class FROM `travel_agency`.`flight`;")
+
+   # "Flight_Carrier, Flight_Fare, Flight_Class, Source_Airport_ID, Destination_Airport_ID "
+
+   return render_template("index.html", flight_carr=flight_carrier.fetchall(), flight_src=flight_source.fetchall(),
+                          flight_dest=flight_destination.fetchall(), flight_c=flight_clas.fetchall())
+
+@app.route('/index.html')
+def index():
+   flight_carrier = mysql.connect().cursor()
+   flight_carrier.execute("SELECT distinct Flight_Carrier FROM `travel_agency`.`flight`;")
+   # flight_carr= flight_carrier.fetchall()
+
+   flight_source = mysql.connect().cursor()
+   flight_source.execute("SELECT distinct Source_Airport_ID FROM `travel_agency`.`flight`;")
+
+   flight_destination = mysql.connect().cursor()
+   flight_destination.execute("SELECT distinct Destination_Airport_ID FROM `travel_agency`.`flight`;")
+
+   flight_clas = mysql.connect().cursor()
+   flight_clas.execute("SELECT distinct Flight_Class FROM `travel_agency`.`flight`;")
+
+   # "Flight_Carrier, Flight_Fare, Flight_Class, Source_Airport_ID, Destination_Airport_ID "
+
+   return render_template("index.html", flight_carr=flight_carrier.fetchall(), flight_src=flight_source.fetchall(),
+                          flight_dest=flight_destination.fetchall(), flight_c=flight_clas.fetchall())
+
+
+
+@app.route('/flight.html')
+def flight():
    # cursor = mysql.connect().cursor()
    # cursor.execute("SELECT * FROM Employee")
    #Flight_Carrier, Flight_Fare, Flight_Class, Source_Airport_ID, Destination_Airport_ID
